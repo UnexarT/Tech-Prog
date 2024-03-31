0f61c09a6698389c3399dfcd45fac805c0df99fd
Daniil <116579474+UnexarT@users.noreply.github.com>
2024-03-31 20:10:26 +0500

    Add files via upload

---
+++ BaseList/List/BaseList.cs
@@ -0,0 +1,84 @@
+﻿abstract class BaseList
+{
+    protected int count;
+
+    public int Count { get { return count; } }
+
+    public abstract void Add(int value);
+
+    public abstract void Insert(int position, int value);
+
+    public abstract void Delete(int position);
+
+    public abstract void Clean();
+
+    public abstract int this[int position] { get; set; }
+
+    public void Print()
+    {
+        Console.Write("[");
+        for (int i = 0; i < count; i++)
+        {
+            if (i == Count - 1) 
+            {
+                Console.Write(this[i]); 
+            }
+            else 
+            { 
+                Console.Write($"{this[i]}, "); 
+            }
+        }
+        Console.WriteLine("]");
+    }
+
+    public void Assign(BaseList source)
+    {
+        Clean();
+        for (int i = 0; i < source.Count; i++)
+        {
+            Add(source[i]);
+        }
+    }
+
+    public void AssignTo(BaseList dest)
+    {
+        dest.Assign(this);
+    }
+
+    public BaseList Clone()
+    {
+        BaseList cloneList = EmptyClone(); // Зачем?
+        cloneList.Assign(this);
+        return cloneList;
+    }
+
+    public virtual void Sort() // Сортировка вставками для ArrayList
+    {
+        for (int i = 1; i < count; i++)
+        {
+            int cur = this[i];
+            int j = i;
+            while (j > 0 && cur < this[j - 1])
+            {
+                this[j] = this[j - 1];
+                j--;
+            }
+            this[j] = cur;
+        }
+    }
+
+    public bool IsEqual(BaseList list)
+    {
+        if (count != list.Count) { return false; }
+        else  
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (this[i] != list[i]) { return false; }
+            }
+            return true;
+        }
+    }
+
+    protected abstract BaseList EmptyClone();
+}
---
+++ BaseList/List/main.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BaseList array = new ArrayList();
+        BaseList chain = new ChainList();
+
+        Random rnd = new Random();
+        for (int i = 0; i < 10000; i++)
+        {
+            int operation = rnd.Next(3);
+            int item1 = rnd.Next(50);
+            int item2 = item1;
+            int pos = rnd.Next(50);
+            switch (operation)
+            {
+                case 1:
+                    array.Add(item1);
+                    chain.Add(item2);
+                    break;
+                case 2:
+                    array.Delete(pos);
+                    chain.Delete(pos);
+                    break;
+                case 3:
+                    array.Insert(pos, item1);
+                    chain.Insert(pos, item2);
+                    break;
+                    /*case 4:
+                        array.Clean();
+                        chain.Clean();
+                        break;
+                    case 5:
+                        array.Sort();
+                        chain.Sort();
+                        break;*/
+            }
+        }
+
+        Console.Write("Изначальный список: ");
+        array.Print();
+
+        // Клонирование
+        BaseList arrayClone = array.Clone();
+        Console.Write("Клонированный список: ");
+        arrayClone.Print();
+        if (arrayClone.IsEqual(array))
+        {
+            Console.WriteLine("Клонированние: идентичны");
+        }
+        else
+        {
+            Console.WriteLine("Клонированние: НЕидентичны");
+        }
+
+        // Сортировка
+        arrayClone.Sort();
+
+        // Упорядоченность
+        bool flag = false;
+        for (int i = 0; i < arrayClone.Count - 1; i++)
+        {
+            if (arrayClone[i] > arrayClone[i + 1])
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            Console.WriteLine("Сорировка: НЕупорядоченна");
+        }
+        else
+        {
+            Console.WriteLine("Сорировка: упорядоченна");
+        }
+
+
+        // Проверка изменения количества символов после сортировки
+        if (array.Count == arrayClone.Count)
+        {
+            Console.WriteLine("Count: НЕ изменилось");
+        }
+        else
+        {
+            Console.WriteLine("Count: изменилось");
+        }
+
+        // Целостность значений после сортировки
+        int k = 0;
+        int j = 0;
+        while (k < array.Count)
+        {
+            while (j < arrayClone.Count)
+            {
+                if (array[k] == arrayClone[j])
+                {
+                    arrayClone.Delete(j);
+                    j = 0;
+                    break;
+                }
+                j++;
+            }
+            k++;
+        }
+        if (arrayClone.Count == 0)
+        {
+            Console.WriteLine("Целостность: НЕ изменилось");
+        }
+        else
+        {
+            Console.WriteLine("Целостность: изменилось");
+        }
+        Console.ReadLine();
+
+       
+    }
+}
